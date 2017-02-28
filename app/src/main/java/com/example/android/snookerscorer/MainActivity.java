66369edd26eaf02660db5a24d35568d5420d4569
mihaaly <mihaaly@gmail.com>
2017-02-28 19:19:14 +0100

    adds experimental behaviour to Next Player button

--- app/src/main/java/com/example/android/snookerscorer/MainActivity.java
+++ app/src/main/java/com/example/android/snookerscorer/MainActivity.java
@@ -3,14 +3,12 @@ package com.example.android.snookerscorer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
-import static com.example.android.snookerscorer.R.id.dot;
-import static com.example.android.snookerscorer.R.id.dot2;
 
 public class MainActivity extends AppCompatActivity {
 
-    int player= 1;
+    int player = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +17,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void nextPlayer (View v) {
-        if (player==1) {
+    public void nextPlayer(View v) {
+        ImageView dot = (ImageView) findViewById(R.id.dot);
+        ImageView dot2 = (ImageView) findViewById(R.id.dot2);
+        if (player == 1) {
             dot.setVisibility(View.INVISIBLE);
+            dot2.setVisibility(View.VISIBLE);
+            player = 2;
         } else {
             dot2.setVisibility(View.INVISIBLE);
+            dot.setVisibility(View.VISIBLE);
+            player = 1;
         }
-        player = 2;
-
-
+        
     }
+
+
 }
+
